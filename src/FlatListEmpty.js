2e7aead9ccb0c7927f39a787613692303eb2d00d
Jim Ji <jimji1005@gmail.com>
2025-11-24 09:21:13 -0800

    add option to have extra string

--- src/FlatListEmpty.js
+++ src/FlatListEmpty.js
@@ -4,11 +4,11 @@ import * as StyledText from './StyledText';
 import Layout from './constants/Layout';
 import PropTypes from 'prop-types';
 
-export default function FlatListEmpty({ width, containerStyle, textStyle, textColor = 'rgba(4, 4, 21, 0.8)' }) {
+export default function FlatListEmpty({ text = 'No results found', width, containerStyle, textStyle, textColor = 'rgba(4, 4, 21, 0.8)' }) {
 	return (
 		<View style={[styles.container, { width: width || Layout.window.tableWidth }, containerStyle]}>
 			<StyledText.Body1 style={textStyle} textColor={textColor}>
-				No results found
+				{text}
 			</StyledText.Body1>
 		</View>
 	);
@@ -32,5 +32,6 @@ FlatListEmpty.propTypes = {
 	width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 	containerStyle: PropTypes.object,
 	textStyle: PropTypes.object,
-	textColor: PropTypes.string
+	textColor: PropTypes.string,
+	text: PropTypes.string
 };
